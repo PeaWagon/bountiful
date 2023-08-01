253b2c2792a1bc18068e4b714742624071848722
Jen <25353074+PeaWagon@users.noreply.github.com>
2023-08-01 18:22:43 +0000

    add extra token response url

--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('login', {});
 });
 
+router.post("/code", (req, res, next) => {
+    console.log(req.headers);
+    console.log(req.statusCode);
+    console.log(req.body);
+    console.log(res.headers);
+    console.log(res.statusCode);
+    console.log(res.body);
+    res.redirect("/");
+});
+
 module.exports = router;
